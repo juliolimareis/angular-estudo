21a5855bcec48655f5af6e8d63927fa7071faaa7
Júlio César Lima Reis <juliolimareis@gmail.com>
2021-07-09 16:37:20 -0300

    add components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,16 @@
+import { CardComponent } from './components/card/card.component';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TopBarComponent } from './components/top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+		TopBarComponent,
+		CardComponent
   ],
   imports: [
     BrowserModule,
